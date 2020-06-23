3ba58465ee0d23297b407b388663d1a754bd66de
Hugo Andreassa <hugo.andreassa@gmail.com>
2020-06-23 18:11:11 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/hyperdrive/course/config/TestConfig.java
+++ src/main/java/com/hyperdrive/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.hyperdrive.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.hyperdrive.course.entities.Order;
 import com.hyperdrive.course.entities.User;
+import com.hyperdrive.course.repositories.OrderRepository;
 import com.hyperdrive.course.repositories.UserRepository;
 
 @Configuration
@@ -16,18 +19,20 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
-
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
 		
-		/*
-		 * userRepository.save(u1);
-		 * userRepository.save(u2);
-		 *  
-		 */
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}	
 }
---
+++ src/main/java/com/hyperdrive/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.hyperdrive.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hyperdrive.course.entities.Order;
+import com.hyperdrive.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> user = repository.findById(id);
+		
+		return user.get();
+	}
+}
